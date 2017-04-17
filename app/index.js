db367b68809f22badee64ea8e30654971ff9617f
Raunaq <raunaqss@gmail.com>
2017-04-17 17:12:49 +0530

    Component Avatar with name, username, and img. Type checking tested with PropTypes.

--- app/index.js
+++ app/index.js
@@ -1,18 +1,34 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello World!
+        <img src={this.props.img}
+            alt='Avatar'
+            style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
+Badge.propTypes = {
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired
+}
+
 ReactDOM.render(
-  <App />,
+  <Badge
+    name='Raunaq Singh'
+    username='raunaqss'
+    img='https://avatars3.githubusercontent.com/u/7628888?v=3&s=460'
+  />,
   document.getElementById('app')
 );
