06ecf4fe3ef810f4aa749eb81c4a93fe3a905374
Raunaq <raunaqss@gmail.com>
2017-04-17 18:41:20 +0530

    Managing & Updating Comp. State: Built flex list with popular languages, clicking list item updates state with selectedLanguage

--- app/index.js
+++ app/index.js
@@ -1,34 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
 require('./index.css');
+var App = require('./components/App');
 
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.img}
-            alt='Avatar'
-            style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired
-}
 
 ReactDOM.render(
-  <Badge
-    name='Raunaq Singh'
-    username='raunaqss'
-    img='https://avatars3.githubusercontent.com/u/7628888?v=3&s=460'
-  />,
+  <App />,
   document.getElementById('app')
 );
