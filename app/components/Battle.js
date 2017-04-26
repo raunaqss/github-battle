ece6a676243648f72937d8b561437b1a0046d1b8
Raunaq <raunaqss@gmail.com>
2017-04-26 14:54:53 +0530

    Video: Composition with props.children: github battle mostly done

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,34 +1,7 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 var Link = require('react-router-dom').Link;
-
-function PlayerPreview(props) {
-  return (
-    <div>
-      <div className='column'>
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-        />
-        <h2 className='username'>@{props.username}</h2>
-      </div>
-      <button
-        className='reset'
-        onClick={props.onReset.bind(null, props.id)}
-      >
-      Reset
-      </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
+var PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -136,9 +109,14 @@ class Battle extends React.Component {
             <PlayerPreview
               avatar={playerOneImg}
               username={playerOneName}
-              id='playerOne'
-              onReset={this.handleReset}
-            />
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}
+              >
+              Reset
+              </button>
+            </PlayerPreview>
           }
           {!playerTwoName &&
             <PlayerInput
@@ -151,9 +129,14 @@ class Battle extends React.Component {
             <PlayerPreview
               avatar={playerTwoImg}
               username={playerTwoName}
-              id='playerTwo'
-              onReset={this.handleReset}
-            />
+            >
+              <button
+                className='reset'
+                onClick={this.handleReset.bind(null, 'playerTwo')}
+              >
+              Reset
+              </button>
+            </PlayerPreview>
           }
         </div>
         {playerOneImg && playerTwoImg &&
--- app/components/Results.js
+++ app/components/Results.js
@@ -1,20 +1,118 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 var queryString = require('query-string')
 var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
+
+function Profile(props) {
+  var info = props.info;
+
+  return (
+    <PlayerPreview
+      avatar={info.avatar_url}
+      username={info.login}
+    >
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired
+}
+
+function Player(props) {
+  return (
+    <div>
+      <h1 className='header'>{props.label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <Profile info={props.profile} />
+    </div>
+  )
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired
+}
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
   componentDidMount() {
     var players = queryString.parse(this.props.location.search);
     api.battle([
       players.playerOneName,
       players.playerTwoName
-    ]).then(function(data) {
-      console.log(data);
-    });
+    ]).then(function(results) {
+      if (results === null) {
+        this.setState(function() {
+          return {
+            error: "There was a problem. One or more users don't exist on Github.",
+            loading: false
+          }
+        });
+      } else {
+        this.setState(function() {
+          return {
+            winner: results[0],
+            loser: results[1],
+            loading: false
+          }
+        });
+      }
+    }.bind(this));
   }
   render() {
+    var error = this.state.error;
+    var loading = this.state.loading;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+
+    if (loading === true) {
+      return <p>Loading</p>
+    }
+    
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <div>Results</div>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+      </div>
     )
   }
 }
